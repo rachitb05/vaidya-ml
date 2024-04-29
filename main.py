f491559834b43f30fa6f574a9027ba8c1a59e76a
rachitb05 <86082020+rachitb05@users.noreply.github.com>
2024-04-29 19:47:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=['https://vaidya32.vercel.app'])
 
 # List of the symptoms is listed here in list l1.
 
